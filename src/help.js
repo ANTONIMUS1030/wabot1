f53bd607428cd9f0bbc2e10afb215a1464ec281a
ANTONIMUS1030 <78993908+ANTONIMUS1030@users.noreply.github.com>
2021-02-15 01:12:19 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,196 @@
+const //database
+	return `
+╔═══════════════════
+║                           
+║     𝕊𝔼ℝ𝔾𝕀ℕ 𝔻𝕆 𝔾ℝ𝔸𝕍𝔼
+║
+╠═══════════════════
+║ 	  
+╠➥ 𝐃𝐎𝐍𝐎:  ⃬⃗ANTONIMUS
+╠➥ wa.me/+5592994604627
+╠➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║ 	    • ──── ✾ ──── •
+║         » NOVIDADES «
+║ 	    • ──── ✾ ──── •
+║ ➸ ${prefix}qrcode [premium]
+║ ➸ ${prefix}chentai [premium]
+║ ➸ ${prefix}gcpf [premium]
+║ ➸ ${prefix}gay [@]
+║ ➸ ${prefix}gbin [premium]
+║ ➸ ${prefix}pack [premium]
+║ ➸ ${prefix}destrava [premium]
+║ ➸ ${prefix}gpessoa [premium]
+║ ➸ ${prefix}wame
+║ ➸ ${prefix}spamcall
+║ ➸ ${prefix}play (nome da msc)
+║
+║ 	    • ──── ✾ ──── •
+║            » MENU «
+║ 	    • ──── ✾ ──── •
+║
+║ ➸ ${prefix}figu
+║ ➸ ${prefix}toimg
+║ ➸ ${prefix}meme
+║ ➸ ${prefix}memeindo
+║ ➸ ${prefix}tts
+║ ➸ ${prefix}lolih [on]
+║ ➸ ${prefix}nsfwloli [off]
+║ ➸ ${prefix}url2img
+║ ➸ ${prefix}leens [na legenda]
+║ ➸ ${prefix}wait [na legenda]
+║ ➸ ${prefix}setprefix
+║ 	    • ──── ✾ ──── •
+║           » OUTROS «
+║ 	    • ──── ✾ ──── •
+║ ➸ ${prefix}linkgp
+║ ➸ ${prefix}simih [1/0]
+║ ➸ ${prefix}marcar*
+║ ➸ ${prefix}add [@]*
+║ ➸ ${prefix}banir [@]*
+║ ➸ ${prefix}promover [@]*
+║ ➸ ${prefix}rebaixar*
+║ ➸ ${prefix}admins*
+║ ➸ ${prefix}marcar2*
+║ ➸ ${prefix}bc [texto]* (ele faz uma ™)
+║ ➸ ${prefix}marcar3*
+║ ➸ ${prefix}bloqueados*
+║ ➸ ${prefix}bloquear [@]*
+║ ➸ ${prefix}desbloquear [@]*
+║ ➸ ${prefix}limpar*
+║ ➸ ${prefix}bc [ *texto* ]*
+║ ➸ ${prefix}bemvindo [1/0]*
+║ ➸ ${prefix}clonar [@]*
+║ ➸ ${prefix}help1*
+║ ➸ ${prefix}dono*
+║ ➸ ${prefix}owner*
+║ ➸ ${prefix}tts [texto]*
+║ ➸ ${prefix}setnome*
+║ ➸ ${prefix}termux*
+║ ➸ ${prefix}setfoto*
+║ ➸ ${prefix}grupoinfo*
+║ ➸ ${prefix}ytmp4*
+║ ➸ ${prefix}bomdia*
+║ ➸ ${prefix}boanoite*
+║ ➸ ${prefix}marcar*
+║ ➸ ${prefix}marcar2*
+║ ➸ ${prefix}marcar3*
+║ 	    • ──── ✾ ──── •
+║          » IMAGENS «
+║ 	    • ──── ✾ ──── •
+║ ➸ ${prefix}loli* [off]
+║ ➸ ${prefix}loli1*
+║ ➸ ${prefix}hentai*
+║ ➸ ${prefix}dono*
+║ ➸ ${prefix}porno*
+║ ➸ ${prefix}boanoite*
+║ ➸ ${prefix}bomdia*
+║ ➸ ${prefix}boatarde*
+║ ➸ ${prefix}mia*
+║ ➸ ${prefix}mia1*
+║ ➸ ${prefix}mia2*
+║ ➸ ${prefix}belle*
+║ ➸ ${prefix}belle1*
+║ ➸ ${prefix}belle2*
+║ ➸ ${prefix}belle3*
+║ ➸ ${prefix}akeno*
+║ ➸ ${prefix}meme*   
+║ ➸ ${prefix}lofi*
+║ ➸ ${prefix}malkova*
+║ ➸ ${prefix}canal*
+║ ➸ ${prefix}nsfwloli1*
+║ ➸ ${prefix}reislin*
+║ 	    • ──── ✾ ──── •
+║      » INTELIGÊNCIA IA «
+║ 	    • ──── ✾ ──── •
+║ ➸ ${prefix}simih 1 (para ativar)*
+║ ➸ ${prefix}simih 0 (para desativar)*
+║ ➸ ${prefix}simi (sua mensagem)*
+║ 	    • ──── ✾ ──── •
+║         » EM TESTE «
+║ 	    • ──── ✾ ──── •
+║ ➸ ${prefix}*
+║ ➸ ${prefix}*
+║ ➸ ${prefix}*
+║ 	    • ──── ✾ ──── •
+║          » PREMIUM «
+║ 	    • ──── ✾ ──── •
+║ ➸ ${prefix}dado*
+║ ➸ ${prefix}cekvip*
+║ ➸ ${prefix}premiumlist*
+║ ➸ ${prefix}delete*
+║ ➸ ${prefix}modapk*
+║ ➸ ${prefix}indo10*
+║ ➸ ${prefix}daftarvip [para virar Premium]*
+║ ➸ ${prefix}qrcode*
+║ ➸ ${prefix}chentai*
+║ ➸ ${prefix}gcpf*
+║ ➸ ${prefix}gbin*
+║ ➸ ${prefix}pack*
+║ ➸ ${prefix}destrava*
+║ ➸ ${prefix}gpessoa*
+║ 	    • ──── ✾ ──── •
+║           » GRUPO «
+║ 	    • ──── ✾ ──── •
+║ ➸ ${prefix}banir*
+║ ➸ ${prefix}leveling [on/off]*
+║ ➸ ${prefix}level*
+║ ➸ ${prefix}add*
+║ ➸ ${prefix}promover*
+║ ➸ ${prefix}setfoto [na legenda]*
+║ ➸ ${prefix}setname [texto]*
+║ ➸ ${prefix}rebaixar*
+║ ➸ ${prefix}admins*
+║ ➸ ${prefix}marcar*
+║ ➸ ${prefix}marcar2*
+║ ➸ ${prefix}marcar3*
+║ ➸ ${prefix}bemvindo [1/0]*
+║ ➸ ${prefix}grupoinfo*
+║ ➸ ${prefix}bomdia*
+║ ➸ ${prefix}boatarde*
+║ ➸ ${prefix}boanoite*
+║ ➸ ${prefix}setdesc*
+║ ➸ ${prefix}bug [sua mensagem]*
+║ 	    • ──── ✾ ──── •
+║    » ESPECIFICO DO BOT «
+║ 	    • ──── ✾ ──── •
+║ ➸ ${prefix}bug [sua mensagem]*
+║ ➸ ${prefix}clonar [@]*
+║ ➸ ${prefix}dono*
+║ ➸ ${prefix}ping [ver velocidade do bot]*
+║ ➸ ${prefix}termux*
+║ ➸ ${prefix}gay [@]*
+║ ➸ ${prefix}wame*
+║ ➸ ${prefix}map (nome)*
+║ ➸ ${prefix}setppbot (marque uma img)*
+║ ➸ ${prefix}pinterest (nome)*
+║ ➸ ${prefix}desligar (so para o dono)*
+║ ➸ ${prefix}timer*
+║ 	    • ──── ✾ ──── •
+║        » MAIS ALGUNS «
+║ 	    • ──── ✾ ──── •
+║ ➸ ${prefix}neko
+║ ➸ ${prefix}ttp [texto]
+║ ➸ ${prefix}testime
+║ ➸ ${prefix}tomp3
+║ ➸ ${prefix}animehug
+║ ➸ ${prefix}shoturl
+║ ➸ ${prefix}pokemon
+║
+║ 	    • ──── ✾ ──── •
+║            » 𝗗𝗢𝗡𝗢 «
+║ 	    • ──── ✾ ──── •
+║
+║  NOME: ANTONIMUS
+║  INSTA: OFF
+║  WPP: wa.me/+5592994604627
+║
+║     Peita ou respeita 🐊🚩
+║    kibadores, vão se fuder
+║       KIBEI MSM FDS 
+║     CANAL DO CRIADOR: 
+║      https://bit.ly/3qAjeNz
+║
+╚═〘 𝕊𝔼ℝ𝔾𝕀ℕ 𝔻𝕆 𝔾ℝ𝔸𝕍𝔼 𝐁𝐎𝐓 〙`
+}
+
+//Online.database
