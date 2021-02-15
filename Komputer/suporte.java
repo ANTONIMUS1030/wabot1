dc6cd41821e386ce0f898d8e2121d69778b08638
ANTONIMUS1030 <78993908+ANTONIMUS1030@users.noreply.github.com>
2021-02-15 00:42:51 -0400

    Add files via upload

---
+++ Komputer/suporte.java
@@ -0,0 +1,5 @@
+Pc = komputer * (wabot3.0 + darkbot - dark)
+           + 4 * bot; // PREFER
+
+Android = /0/android/suporte.java * (wabot3.0 + dark
+                       - darkbot) + 4 * bot; // AVOID
