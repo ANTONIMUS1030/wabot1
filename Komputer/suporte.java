f2c7c9d64e3c2adc3122d0062e1cefca287f33f0
ANTONIMUS1030 <78993908+ANTONIMUS1030@users.noreply.github.com>
2021-03-30 14:48:15 -0400

    Update suporte.java

--- Komputer/suporte.java
+++ Komputer/suporte.java
@@ -1,5 +1,5 @@
-Pc = komputer * (wabot1 + wabot1 - dark)
+Pc = komputer * (wabot1 + wabot1 - ATNMS)
            + 4 * bot; // PREFER
 
-Android = /0/android/suporte.java * (wabot1 + dark
+Android = /0/android/suporte.java * (wabot1 + ATNMS
                        - wabot1) + 4 * bot; // AVOID
