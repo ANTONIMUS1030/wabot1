d5bb42dc66331301229e4bdbb4ae0364b749676a
ANTONIMUS1030 <78993908+ANTONIMUS1030@users.noreply.github.com>
2021-10-15 20:57:56 -0400

    suporte.java

--- Komputer/suporte.java
+++ Komputer/suporte.java
@@ -1,5 +1,5 @@
-Pc = komputer * (wabot1 + wabot1 - ATNMS)
+Pc = komputer * (wabot1 + wabot1 - MTH)
            + 4 * bot; // PREFER
 
-Android = /0/android/suporte.java * (wabot1 + ATNMS
+Android = /0/android/suporte.java * (wabot1 + MTH
                        - wabot1) + 4 * bot; // AVOID
